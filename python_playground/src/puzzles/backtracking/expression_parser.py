4c30699aa01f394505bde9a356e6438a2599edfd
bigfrog10 <curiosgaga@gmail.com>
2020-08-21 19:35:45 -0400

    expression parser

--- python_playground/src/puzzles/backtracking/expression_parser.py
+++ python_playground/src/puzzles/backtracking/expression_parser.py
@@ -1,3 +1,5 @@
+import queue
+import re
 # https://blog.csdn.net/iteye_15612/article/details/81725858
 # https://www.chris-j.co.uk/parsing.php
 # https://www.cs.cmu.edu/~adamchik/15-121/lectures/Stacks%20and%20Queues/Stacks%20and%20Queues.html
@@ -10,8 +12,6 @@
 # If a token is a right parentheses ')', you pop entries until you meet '('.
 # When you finish reading the string, you pop up all tokens which are left there.
 # Arithmetic precedence is in increasing order: '+', '-', '*', '/';
-import queue
-import re
 
 number_or_symbol = re.compile(r'(\d+|[^ 0-9])')
 tokens = re.findall(number_or_symbol, '(81 * 6) / 42 + (3 - 1)')
