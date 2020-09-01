fc6b41457456f30f5ef0c551512e95597e27d85c
psilons <cuirick@gmail.com>
2020-09-01 01:05:39 -0400

    improve performance

--- python_playground/src/puzzles/backtracking/klotski.py
+++ python_playground/src/puzzles/backtracking/klotski.py
@@ -57,14 +57,14 @@ class SlidingBlock:
                              for i in range(self.length) for j in range(self.width)]
         return self.occupied
 
-    def is_overlaid(self, sliding_block):
-        if not self.occupied:
-            self.occupied = [(self.x_coord + i, self.y_coord + j)
-                             for i in range(self.length) for j in range(self.width)]
-
-        other_occupied = sliding_block.occupied_spaces()
-        intersection = set(self.occupied) & set(other_occupied)
-        return len(intersection) > 0
+    # def is_overlaid(self, sliding_block):
+    #     if not self.occupied:
+    #         self.occupied = [(self.x_coord + i, self.y_coord + j)
+    #                          for i in range(self.length) for j in range(self.width)]
+    #
+    #     other_occupied = sliding_block.occupied_spaces()
+    #     intersection = set(self.occupied) & set(other_occupied)
+    #     return len(intersection) > 0
 
     def is_occupied(self, x, y):
         return self.x_coord <= x < self.x_coord + self.length and self.y_coord <= y < self.y_coord + self.width
@@ -91,7 +91,7 @@ class SlidingGameBoard:
 
         self.shape_to_num = {}
         counter = 0  # 0 indicates empty
-        for block in sliding_blocks:
+        for block in sliding_blocks:  # same shape should have same number, so reduce branches
             exist_num = self.shape_to_num.get((block.length, block.width), None)
             if not exist_num:
                 counter += 1
@@ -105,14 +105,14 @@ class SlidingGameBoard:
 
         self.layout = None
 
-    def is_out_of_bounds(self, sliding_block):
-        for space in sliding_block.occupied_spaces():
-            if space[0] < 0 or space[0] >= self.length:
-                return True
-            if space[1] < 0 or space[1] >= self.width:
-                return True
-
-        return False
+    # def is_out_of_bounds(self, sliding_block):
+    #     for space in sliding_block.occupied_spaces():
+    #         if space[0] < 0 or space[0] >= self.length:
+    #             return True
+    #         if space[1] < 0 or space[1] >= self.width:
+    #             return True
+    #
+    #     return False
 
     def get_layout(self):
         if not self.layout:
@@ -124,21 +124,21 @@ class SlidingGameBoard:
 
         return self.layout
 
-    def is_overlaid(self, sliding_block):
-        for block in self.sliding_blocks:
-            if block == sliding_block:
-                continue
-
-            if block.is_overlaid(sliding_block):
-                return True
-
-        return False
+    # def is_overlaid(self, sliding_block):
+    #     for block in self.sliding_blocks:
+    #         if block == sliding_block:
+    #             continue
+    #
+    #         if block.is_overlaid(sliding_block):
+    #             return True
+    #
+    #     return False
 
     def __str__(self):
-        return str(self.steps)
+        return str(self.steps) + ', ' + str(self.layout)
 
     def __repr__(self):  # without this, during debug, list of this class does not show str.
-        return str(self.steps)
+        return str(self.steps) + ', ' + str(self.layout)
 
     def deep_clone(self):
         # copy.deepcopy and pickle are slow since we have internal states
@@ -161,7 +161,7 @@ class SlidingGameBoard:
 
         ret.zobrist_tbl = self.zobrist_tbl  # random is slow
 
-        ret.layout = None
+        ret.layout = None  # don't copy this field since block is moved.
         return ret
 
 
@@ -187,61 +187,71 @@ def solve(board: SlidingGameBoard):
     queue = collections.MyQueue()
     queue.enqueue(board)
     while queue:
-        board1 = queue.dequeue()
-        print('steps={}, queue size={}'.format(len(board1.steps), len(queue)))
+        board = queue.dequeue()
+        print('queue size={}, steps={}'.format(len(queue), len(board.steps)))
 
-        if len(board1.steps) >= 150:
-            continue
-
-        for block in board1.sliding_blocks:
+        for block in board.sliding_blocks:
             # we could use Direction for loop to shorten this
-            move_to(Direction.UP, block, board1, cache, queue, results)
-            move_to(Direction.DOWN, block, board1, cache, queue, results)
-            move_to(Direction.LEFT, block, board1, cache, queue, results)
-            move_to(Direction.RIGHT, block, board1, cache, queue, results)
+            if is_movable(Direction.UP, block, board):
+                move_to(Direction.UP, block, board, cache, queue, results)
+            if is_movable(Direction.DOWN, block, board):
+                move_to(Direction.DOWN, block, board, cache, queue, results)
+            if is_movable(Direction.LEFT, block, board):
+                move_to(Direction.LEFT, block, board, cache, queue, results)
+            if is_movable(Direction.RIGHT, block, board):
+                move_to(Direction.RIGHT, block, board, cache, queue, results)
 
     return results
 
 
-def move_to(direction, block, board1, cache, queue, results):
-    layout = board1.get_layout()
+# for performance reason, we spill out details, so lengthy
+def is_movable(direction, block, board):
+    layout = board.get_layout()
     if direction == Direction.UP:
         if block.x_coord == 0:
-            return
+            return False
+        top_row = layout[block.x_coord-1]
         for i in range(block.width):
-            if block.y_coord + i < board1.width and layout[block.x_coord-1][block.y_coord + i] != 0:
-                return
+            if block.y_coord + i < board.width and top_row[block.y_coord + i] != 0:  # 0 means empty
+                return False
     elif direction == Direction.DOWN:
-        if block.x_coord + block.length == board1.length:
-            return
+        if block.x_coord + block.length == board.length:
+            return False
+        bottom_row = layout[block.x_coord+block.length]
         for i in range(block.width):
-            if block.y_coord + i < board1.width and layout[block.x_coord+block.length][block.y_coord + i] != 0:
-                return
+            if block.y_coord + i < board.width and bottom_row[block.y_coord + i] != 0:
+                return False
     elif direction == Direction.LEFT:
         if block.y_coord == 0:
-            return
+            return False
         for i in range(block.length):
-            if block.x_coord + i < board1.length and layout[block.x_coord+i][block.y_coord - 1] != 0:
-                return
+            if block.x_coord + i < board.length and layout[block.x_coord + i][block.y_coord - 1] != 0:
+                return False
     elif direction == Direction.RIGHT:
-        if block.y_coord + block.width == board1.width:
-            return
+        if block.y_coord + block.width == board.width:
+            return False
         for i in range(block.length):
-            if block.x_coord + i < board1.length and layout[block.x_coord+i][block.y_coord + block.width] != 0:
-                return
+            if block.x_coord + i < board.length and layout[block.x_coord + i][block.y_coord + block.width] != 0:
+                return False
 
+    return True
+
+
+def move_to(direction, block, board, cache, queue, results):
     block.move(direction)
     # if not board1.is_out_of_bounds(block) and not board1.is_overlaid(block):
-    new_board = board1.deep_clone()
+    new_board = board.deep_clone()
     new_board.steps.append((block.name, block.x_coord, block.y_coord, direction.name))
-    hb = hash_board(new_board)
-    if new_board.finish_criteria(new_board):
-        results.append(new_board)
-    elif hb not in cache:
-        cache.add(hb)
-        queue.enqueue(new_board)
 
-    block.move(direction.opposite())
+    if new_board.finish_criteria(new_board):  # if it is a solution, add to result and stop
+        results.append(new_board)
+    else:
+        hb = hash_board(new_board)
+        if hb not in cache:  # check if it is already dealt with. If not, add it as new case.
+            cache.add(hb)
+            queue.enqueue(new_board)
+
+    block.move(direction.opposite())  # restore to last state for next try
 
 
 # ## testing
@@ -278,6 +288,16 @@ def test():
         print(len(s.steps))
         print(s)
 
+
+# from pyinstrument import Profiler
+#
+# profiler = Profiler()
+# profiler.start()
+#
+# test()
+#
+# profiler.stop()
+# print(profiler.output_text(unicode=True, color=True))
 
 import cProfile
 cProfile.run('test()')
