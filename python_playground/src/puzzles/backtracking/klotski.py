8d20af0f46016c607b3ee2ed67f5460ea4abcd31
psilons <cuirick@gmail.com>
2020-09-01 02:02:55 -0400

    improve performance

--- python_playground/src/puzzles/backtracking/klotski.py
+++ python_playground/src/puzzles/backtracking/klotski.py
@@ -34,22 +34,14 @@ class SlidingBlock:
     def move(self, direction):
         if direction == Direction.UP:
             self.x_coord -= 1
-            self.occupied = []
-            return self.x_coord, self.y_coord
         elif direction == Direction.LEFT:
             self.y_coord -= 1
-            self.occupied = []
-            return self.x_coord, self.y_coord
         elif direction == Direction.RIGHT:
             self.y_coord += 1
-            self.occupied = []
-            return self.x_coord, self.y_coord
         elif direction == Direction.DOWN:
             self.x_coord += 1
-            self.occupied = []
-            return self.x_coord, self.y_coord
 
-        return None
+        self.occupied = []
 
     def occupied_spaces(self):
         if not self.occupied:
@@ -57,20 +49,11 @@ class SlidingBlock:
                              for i in range(self.length) for j in range(self.width)]
         return self.occupied
 
-    # def is_overlaid(self, sliding_block):
-    #     if not self.occupied:
-    #         self.occupied = [(self.x_coord + i, self.y_coord + j)
-    #                          for i in range(self.length) for j in range(self.width)]
-    #
-    #     other_occupied = sliding_block.occupied_spaces()
-    #     intersection = set(self.occupied) & set(other_occupied)
-    #     return len(intersection) > 0
-
     def is_occupied(self, x, y):
         return self.x_coord <= x < self.x_coord + self.length and self.y_coord <= y < self.y_coord + self.width
 
     def __str__(self):
-        return '{name={}, x={}, y={}, len={}, wid={}}'.format(
+        return '{name={}, x_coord={}, y_coord={}, length={}, width={}}'.format(
             self.name, self.x_coord, self.y_coord, self.length, self.width)
 
     def __repr__(self):
@@ -98,21 +81,8 @@ class SlidingGameBoard:
                 self.shape_to_num[(block.length, block.width)] = counter
 
         self.steps = []
-        # this field shouldn't be here, need to move to solver
-        self.zobrist_tbl = [[[random.randint(1, 2**64 - 1)
-                              for _ in range(len(self.shape_to_num)+1)]  # 1 for empty space case
-                             for _ in range(width)] for _ in range(length)]
 
         self.layout = None
-
-    # def is_out_of_bounds(self, sliding_block):
-    #     for space in sliding_block.occupied_spaces():
-    #         if space[0] < 0 or space[0] >= self.length:
-    #             return True
-    #         if space[1] < 0 or space[1] >= self.width:
-    #             return True
-    #
-    #     return False
 
     def get_layout(self):
         if not self.layout:
@@ -124,15 +94,49 @@ class SlidingGameBoard:
 
         return self.layout
 
-    # def is_overlaid(self, sliding_block):
-    #     for block in self.sliding_blocks:
-    #         if block == sliding_block:
-    #             continue
-    #
-    #         if block.is_overlaid(sliding_block):
-    #             return True
-    #
-    #     return False
+    def hash_board(self, zobrist_tbl):
+        hb = self.get_layout()
+
+        ret = 0
+        for i in range(self.length):
+            for j in range(self.width):
+                ret ^= zobrist_tbl[i][j][hb[i][j]]
+
+        # we could also add symmetric board as well if we know board is symmetric/door location
+        # but rush hour game is not symmetric, so we leave this optimization out.
+        return ret
+
+    # for performance reason, we spill out details, so lengthy
+    def is_movable(self, direction, block):
+        layout = self.get_layout()
+        if direction == Direction.UP:
+            if block.x_coord == 0:  # out of bound check
+                return False
+            top_row = layout[block.x_coord-1]  # overlay check
+            for i in range(block.width):
+                if block.y_coord + i < self.width and top_row[block.y_coord + i] != 0:  # 0 means empty
+                    return False
+        elif direction == Direction.DOWN:
+            if block.x_coord + block.length == self.length:
+                return False
+            bottom_row = layout[block.x_coord + block.length]
+            for i in range(block.width):
+                if block.y_coord + i < self.width and bottom_row[block.y_coord + i] != 0:
+                    return False
+        elif direction == Direction.LEFT:
+            if block.y_coord == 0:
+                return False
+            for i in range(block.length):
+                if block.x_coord + i < self.length and layout[block.x_coord + i][block.y_coord - 1] != 0:
+                    return False
+        elif direction == Direction.RIGHT:
+            if block.y_coord + block.width == self.width:
+                return False
+            for i in range(block.length):
+                if block.x_coord + i < self.length and layout[block.x_coord + i][block.y_coord + block.width] != 0:
+                    return False
+
+        return True
 
     def __str__(self):
         return str(self.steps) + ', ' + str(self.layout)
@@ -156,97 +160,44 @@ class SlidingGameBoard:
         ret.finish_criteria = self.finish_criteria
 
         ret.steps = self.steps.copy()
-
         ret.shape_to_num = self.shape_to_num
-
-        ret.zobrist_tbl = self.zobrist_tbl  # random is slow
-
         ret.layout = None  # don't copy this field since block is moved.
         return ret
-
-
-def hash_board(board: SlidingGameBoard):
-    hb = board.get_layout()
-
-    ret = 0
-    for i in range(board.length):
-        for j in range(board.width):
-            ret ^= board.zobrist_tbl[i][j][hb[i][j]]
-
-    # we could also add symmetric board as well if we know board is symmetric/door location
-    # but rush hour game is not symmetric, so we leave this optimization out.
-    return ret
 
 
 def solve(board: SlidingGameBoard):
     print("board shape: len={}, wid={}".format(board.length, board.width))
 
+    zobrist_tbl = [[[random.randint(1, 2**64 - 1)
+                     for _ in range(len(board.shape_to_num)+1)]  # 1 for empty space case
+                    for _ in range(board.width)] for _ in range(board.length)]
+    dirs = list(Direction)  # avoid regenerate list everytime, for looping all directions
     results = []
-    cache = {hash_board(board)}
-    # use queue because we need to find the shortest path
-    queue = collections.MyQueue()
+    cache = {board.hash_board(zobrist_tbl)}  # filter out explored cases, cut branches
+    queue = collections.MyQueue()  # this means breathe first search (BFS), find shortest paths
     queue.enqueue(board)
     while queue:
         board = queue.dequeue()
         print('queue size={}, steps={}'.format(len(queue), len(board.steps)))
 
         for block in board.sliding_blocks:
-            # we could use Direction for loop to shorten this
-            if is_movable(Direction.UP, block, board):
-                move_to(Direction.UP, block, board, cache, queue, results)
-            if is_movable(Direction.DOWN, block, board):
-                move_to(Direction.DOWN, block, board, cache, queue, results)
-            if is_movable(Direction.LEFT, block, board):
-                move_to(Direction.LEFT, block, board, cache, queue, results)
-            if is_movable(Direction.RIGHT, block, board):
-                move_to(Direction.RIGHT, block, board, cache, queue, results)
+            for d in dirs:  # loop through UP, DOWN, LEFT and RIGHT in Direction
+                if board.is_movable(d, block):
+                    move_to(d, block, board, cache, queue, results, zobrist_tbl)
 
     return results
 
 
-# for performance reason, we spill out details, so lengthy
-def is_movable(direction, block, board):
-    layout = board.get_layout()
-    if direction == Direction.UP:
-        if block.x_coord == 0:
-            return False
-        top_row = layout[block.x_coord-1]
-        for i in range(block.width):
-            if block.y_coord + i < board.width and top_row[block.y_coord + i] != 0:  # 0 means empty
-                return False
-    elif direction == Direction.DOWN:
-        if block.x_coord + block.length == board.length:
-            return False
-        bottom_row = layout[block.x_coord+block.length]
-        for i in range(block.width):
-            if block.y_coord + i < board.width and bottom_row[block.y_coord + i] != 0:
-                return False
-    elif direction == Direction.LEFT:
-        if block.y_coord == 0:
-            return False
-        for i in range(block.length):
-            if block.x_coord + i < board.length and layout[block.x_coord + i][block.y_coord - 1] != 0:
-                return False
-    elif direction == Direction.RIGHT:
-        if block.y_coord + block.width == board.width:
-            return False
-        for i in range(block.length):
-            if block.x_coord + i < board.length and layout[block.x_coord + i][block.y_coord + block.width] != 0:
-                return False
-
-    return True
-
-
-def move_to(direction, block, board, cache, queue, results):
+def move_to(direction, block, board, cache, queue, results, zobrist_tbl):
     block.move(direction)
-    # if not board1.is_out_of_bounds(block) and not board1.is_overlaid(block):
+
     new_board = board.deep_clone()
     new_board.steps.append((block.name, block.x_coord, block.y_coord, direction.name))
 
     if new_board.finish_criteria(new_board):  # if it is a solution, add to result and stop
         results.append(new_board)
     else:
-        hb = hash_board(new_board)
+        hb = new_board.hash_board(zobrist_tbl)
         if hb not in cache:  # check if it is already dealt with. If not, add it as new case.
             cache.add(hb)
             queue.enqueue(new_board)
