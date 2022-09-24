d5d44ce1a01c86de0a3dade595d2da98a0ed6c1a
xyc-xyc <xyc.xyc@gmail.com>
2022-09-24 15:43:09 -0400

    add more test cases

--- src/tutorials/chapter_05_functions/hidding.py
+++ src/tutorials/chapter_05_functions/hidding.py
@@ -5,17 +5,19 @@
 # 例如A小于B的三分之一，可将A藏于B, 如果又存在B是C的三分之一，不可再将B藏于C。
 
 # 现在已知我方共有n支部队，且知道每支部队的人数，请问，在最优方案下，我们暴露给敌人的部队数量有几支。
-# If we have 15, 9, 4, 2, if we hide 2 under 15, then we can hide 4 under 9.
+# If we have 15, 9, 4, 2, if we hide 2 under 15, then we cannot hide 4 under 9.
 # So we need to hide largest possible under largest.
+# However, if we have 18, 6, 4, 2, if we hide 6 under 18, then we cannot hide 2 under 6.
+# So we need to scan troops with fewer soldiers first and hide it within another of smallest but qualified
 # assume that we can only hide one number under another.
 def hide(int_arr):
-    arr = sorted(int_arr, reverse=True)
+    arr = sorted(int_arr)
     size = len(arr)
 
     flags = [-1] * size
     for i in range(size):
         v = arr[i]
-        c = [ind+i+1 for ind, x in enumerate(arr[i+1:]) if flags[ind] == -1 and x <= v / 3]
+        c = [ind+i+1 for ind, x in enumerate(arr[i+1:]) if flags[ind] == -1 and x >= v * 3]
         if c:
             flags[c[0]] = i
 
@@ -23,3 +25,38 @@ def hide(int_arr):
 
 
 print(hide([2, 9, 4, 15]))
+print(hide([2, 5, 6, 18]))
+
+# use binary search to replace linear search
+from bisect import bisect_left
+def hide2(int_arr):
+    arr = sorted(int_arr)
+    size = len(arr)
+
+    flags = [-1] * size
+    for i in range(size):
+        v = arr[i]*3
+        p = bisect_left(arr, v, lo=i+1)
+        if p < size and arr[p] != v:
+            p += 1
+        if p < size:
+            flags[p] = i
+
+    return list(zip(arr, flags)) 
+
+print(hide2([2, 5, 6, 18]))
+
+# above optimization is to minimize the total number of troops exposed
+
+# Further open question: 
+# if we want to miniize the total number of soldiers exposed, where assume 
+# the number of soldiers are hidden if and only if the troop is nested
+# i.e. 
+# If we have 15, 9, 4, 2, if we hide 4 under 15, 2 under 9, exposed soldiers are 24=18+9
+# If we have 18, 6, 5, 2, 
+#   if we hide 6 under 18, exposed soldiers are 25=18+5+2
+#   if we hide 2 under 6 and 5 under 18, exposed soldiers are 24=18+6
+# If we have 18, 6, 3, 2, 
+#   if we hide 6 under 18, exposed soldiers are 23=18+3+2
+#   if we hide 2 under 6 and 5 under 18, exposed soldiers are 24=18+6
+# it seems no greedy algorithm for this
